6effb8e8393720602ace1d4a35b6b4ae304dd2bc
Ethan Kreloff <ekreloff@ridefare.com>
2017-05-08 16:01:07 -0600

    Initial Commit

---
+++ BackgroundLocationTestTests/BackgroundLocationTestTests.swift
@@ -0,0 +1,36 @@
+//
+//  BackgroundLocationTestTests.swift
+//  BackgroundLocationTestTests
+//
+//  Created by Ethan Kreloff on 5/8/17.
+//  Copyright © 2017 10-4 Systems. All rights reserved.
+//
+
+import XCTest
+@testable import BackgroundLocationTest
+
+class BackgroundLocationTestTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
